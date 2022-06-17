f5d04e834e2f2b8c3b15940baf65408965217606
Volodymyr Fouchek <vinvova555333@gmail.com>
2022-06-17 22:04:43 +0300

    Add Tariff section

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@ function burgerMenu() {
   const burger = document.querySelector('.burger');
   const menu = document.querySelector('.menu');
   const body = document.querySelector('body');
+
   burger.addEventListener('click', () => {
     if (!menu.classList.contains('active')) {
       menu.classList.add('active');
@@ -16,7 +17,7 @@ function burgerMenu() {
       body.classList.remove('locked');
     }
   });
-  // Вот тут мы ставим брейкпоинт навбара
+
   window.addEventListener('resize', () => {
     if (window.innerWidth > 991.98) {
       menu.classList.remove('active');
@@ -27,16 +28,20 @@ function burgerMenu() {
 }
 burgerMenu();
 
-// Вызываем эту функцию, если нам нужно зафиксировать меню при скролле.
-function fixedNav() {
-  const nav = document.querySelector('nav');
+// Fixed nav
+let breakpoint = window.screen.width;
+const getHeigthHeader = () => (window.screen.width >= 1024 ? (breakpoint = 45) : (breakpoint = 34));
 
-  // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
-  const breakpoint = 34;
+getHeigthHeader();
+
+const fixedNav = () => {
+  const nav = document.querySelector('nav');
   if (window.scrollY >= breakpoint) {
     nav.classList.add('fixed__nav');
   } else {
     nav.classList.remove('fixed__nav');
   }
-}
+};
+
 window.addEventListener('scroll', fixedNav);
+window.addEventListener('resize', getHeigthHeader);
