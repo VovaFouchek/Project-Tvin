1103a6ea42102f00cbe62678af23f1cac446c569
Volodymyr Fouchek <vinvova555333@gmail.com>
2022-08-09 12:55:26 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -295,6 +295,25 @@ if (window.screen.width > 600) {
   tooglePopUp('.technical__support-mobile', '.support', '.support__close');
 }
 
+// // Аккордеон
+function accordion() {
+  const items = document.querySelectorAll('.accordion__item-trigger');
+  items.forEach(item => {
+    item.addEventListener('click', () => {
+      const parent = item.parentNode;
+      if (parent.classList.contains('accordion__item-active')) {
+        parent.classList.remove('accordion__item-active');
+      } else {
+        document
+          .querySelectorAll('.accordion__item')
+          .forEach(child => child.classList.remove('accordion__item-active'));
+        parent.classList.add('accordion__item-active');
+      }
+    });
+  });
+}
+accordion();
+;
 /**
  * Swiper 8.2.4
  * Most modern mobile touch slider and framework with hardware accelerated transitions
