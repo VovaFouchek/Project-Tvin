af2544243bfe9f546398314d93cbdc6c4ac98840
Volodymyr Fouchek <vinvova555333@gmail.com>
2022-09-14 18:42:44 +0300

    Updates

--- js/scriptsForm.js
+++ js/scriptsForm.js
@@ -120,13 +120,53 @@ if (window.screen.width > 600) {
 }
 
 const form = document.querySelector('form');
-
+const inner = document.querySelector('.inner');
+const loader = document.querySelector('.lds-roller');
+const inputs = document.querySelectorAll('input');
 // const url = 'https://crm.tvin.com.ua/api/v1/website_callback';
 // const token = 'p3B2GFgbUSRT00lV7YeBf4SDEAjtcdGz';
-const url = 'https://webhook.site/50defbeb-9263-4302-aa4b-875ec2b292cc';
+// const url = 'https://webhook.site/50defbeb-9263-4302-aa4b-875ec2b292cc';
+
+const statusLoading = () => {
+  loader.classList.add('active');
+  form.classList.add('disabled');
+};
+
+const statusFailure = () => {
+  const messageFailure = 'Ой, халепа. Будь ласка, спробуйте ще раз';
+  loader.classList.remove('active');
+  form.classList.remove('disabled');
+  const statusMessage = document.createElement('div');
+  statusMessage.classList.add('status');
+  form.appendChild(statusMessage);
+  statusMessage.textContent = messageFailure;
+};
+
+const statusSuccess = () => {
+  loader.classList.remove('active');
+  document.querySelector('.form__wrap').remove();
+  const statusSuccessMessage = document.createElement('div');
+  statusSuccessMessage.classList.add('.successPopUp');
+  document.querySelector('.successPopUp').classList.add('success');
+  inner.appendChild(statusSuccessMessage);
+};
+
+const message = {
+  loading: statusLoading,
+  success: statusSuccess,
+  failure: statusFailure,
+};
+
+const clearValues = () => {
+  document.querySelector('#contact-method').value = '';
+  inputs.forEach(item => {
+    item.value = '';
+  });
+};
 
 const handleSubmit = event => {
   event.preventDefault();
+  message.loading();
   const userName = document.querySelector('#name').value;
   const userAddress = document.querySelector('#address').value;
   const userContact = document.querySelector('#contact').value;
@@ -145,17 +185,23 @@ const handleSubmit = event => {
 const addInfoToDatabase = async personalData => {
   const fetchData = {
     method: 'POST',
-    body: personalData,
+    body: JSON.stringify(personalData),
+    mode: 'no-cors',
     headers: new Headers({ 'content-type': 'application/x-www-form-urlencoded' }),
     // headers: new Headers({ 'content-type': 'application/x-www-form-urlencoded', 'X-Auth-Token': token }),
   };
-
   try {
-    const response = await fetch(url, fetchData);
-    return response.data;
+    const response = await fetch(url, fetchData)
+      .then(response => response.json())
+      .then(message.success());
   } catch (error) {
-    console.log('Oops.. something go wrong');
-    throw new Error(error);
+    console.error(error);
+    message.failure();
+    setTimeout(() => {
+      document.querySelector('.status').remove();
+    }, 5000);
+  } finally {
+    clearValues();
   }
 };
 
