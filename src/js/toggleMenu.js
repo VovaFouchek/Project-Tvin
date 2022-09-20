b5442d5cf1979e6bd9e9654f6113bcb054e6183c
Volodymyr Fouchek <vinvova555333@gmail.com>
2022-09-20 12:56:32 +0300

    Fix

--- src/js/toggleMenu.js
+++ src/js/toggleMenu.js
@@ -70,5 +70,4 @@ linkTechSupportMobile.addEventListener('click', event => {
   const techSupportBtn = document.querySelector('.technical__support-mobile');
   techSupportBtn.classList.toggle('active');
   document.querySelector('.support').classList.toggle('active');
-  console.log(document.querySelector('.support'));
 });
